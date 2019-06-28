c8350eb39e9f2ae9dc6f9a3b866973871f317a14
Kobayashi-takumi <takumi.kobayashi.development@gmail.com>
2019-06-28 18:13:09 +0900

    first day

--- src/components/SignIn.vue
+++ src/components/SignIn.vue
@@ -1,23 +1,30 @@
 <template>
     <div id="sign-in">
         <v-app>
-            <v-layout align-center justify-center>
-                <v-flex xs12 md4>
-                    <v-card>
-                        <v-card-title>
-                            <div >
-                                <h3 class="headline mb-0">Sign In</h3>
-                            </div>
-                        </v-card-title>
-
-                        <v-card-actions>
-                            <v-btn dark large color="cyan">
-                                <v-icon dark>android</v-icon>
-                            </v-btn>
-                        </v-card-actions>
-                    </v-card>
-                </v-flex>
-            </v-layout>
+            <v-container fluid>
+                <v-layout align-center justify-center row>
+                    <v-flex xs12 md4>
+                        <v-card class="box-card">
+                            
+                            <v-layout justify-center align-content-space-around column>
+                                <v-flex align-self-center>
+                                    <v-card-title>
+                                        <h3 class="headline mb-0">Sign In</h3>
+                                    </v-card-title>
+                                </v-flex>
+                                <v-flex class='pb-0'>
+                                    <v-card-actions>
+                                        <v-btn dark large color="cyan">
+                                            <v-icon dark>android</v-icon>
+                                        </v-btn>
+                                    </v-card-actions>
+                                </v-flex>
+                            </v-layout>
+                            
+                        </v-card>
+                    </v-flex>
+                </v-layout>
+            </v-container>
         </v-app>
     </div>
 </template>
@@ -27,6 +34,9 @@ export default {
     name: 'sign-in'
 }
 </script>
-<style>
-
+<style scoped>
+.box-card {
+    border-radius: 10px;
+    height: 300px;
+}
 </style>
