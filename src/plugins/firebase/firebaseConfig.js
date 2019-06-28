4aa998207ad8f5553a12bc141806296ee8d7aa5d
Kobayashi-takumi <takumi.kobayashi.development@gmail.com>
2019-06-28 16:03:03 +0900

    write gitignore

--- src/plugins/firebase/firebaseConfig.js
+++
@@ -1,9 +0,0 @@
-export default {
-    apiKey: "AIzaSyAU5Kp3c6iV1jEhJ1GBz5G_eOqEKfr6v68",
-    authDomain: "trace-6f88a.firebaseapp.com",
-    databaseURL: "https://trace-6f88a.firebaseio.com",
-    projectId: "trace-6f88a",
-    storageBucket: "",
-    messagingSenderId: "821995377628",
-    appId: "1:821995377628:web:9142c8f780b95118"
-};
